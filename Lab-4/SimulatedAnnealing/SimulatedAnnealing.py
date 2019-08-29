d7cd353a0cdd82681db649d8bf0ded906d6a941f
arundhati24 <arundhatigupta2411@gmail.com>
2019-08-29 20:59:51 +0530

    Format code for question 2

--- Lab-4/SimulatedAnnealing/SimulatedAnnealing.py
+++ Lab-4/SimulatedAnnealing/SimulatedAnnealing.py
@@ -7,17 +7,20 @@ from State import State
 from output_util import output_result
 import sys
 
-visited_state = {}
-state_parent = {}
+# Keeps track of visited states
+state_visited = {}
+
+# Keeps track of parent state
+parent_state = {}
 
 class simulated_annealing:
     def __init__(self, intial_state, goal_state, temperature):
+        self.temperature = temperature
+        self.max_temperature = temperature
         self.initial_configuration = intial_state
         self.final_configuration = goal_state
-        self.temperature = temperature
-        self.temperature_max = temperature
-        self.x = -temperature / 5
         self.step = 0.0000001
+        self.x = -temperature / 5
         self.state_explored = 0
         sys.setrecursionlimit(181440)
 
@@ -28,8 +31,8 @@ class simulated_annealing:
         return {
             1: self.linear_strategy(),
             2: self.random_strategy(),
-            3: self.neg_exponential(),
-            4: self.pos_exponential(),
+            3: self.negative_exponential(),
+            4: self.positive_exponential(),
         }[choice]
 
     def solve_eight_puzzle(self, current_state, heuristic_choice, cooling_choice):
@@ -39,63 +42,63 @@ class simulated_annealing:
             print(self.state_explored , current_state)
             if current_state == self.final_configuration:
                 self.state_explored += 1
-                out = output_result(self.initial_configuration, "simulated_annealing_result.txt", state_parent,
+                out = output_result(self.initial_configuration, "simulated_annealing_result.txt", parent_state,
                                     self.state_explored)
                 out.write_output_path(current_state)
-                print("Goal Achieved....")
+                print("Goal Achieved!")
                 return 0
-            elif current_state in visited_state:
+            elif current_state in state_visited:
                 continue
             else:
                 self.cooling_temperature(cooling_choice)
                 self.state_explored += 1
-                visited_state[current_state] = 1
+                state_visited[current_state] = 1
                 state = State(current_state, Heuristic(self.final_configuration).getHeuristicEstimation(
                     current_state, heuristic_choice
                 ))
                 neighbours = state.getAllSuccessor(heuristic_choice)
                 neighbours.sort()
-                idx, cur = 0, 0
-                sz = len(neighbours)
-                li = []
-                mark = [0]*sz
-                cnt = 0
-                while cnt < len(neighbours):
-                    e = self.energy_difference(state, neighbours[cur])
-                    if mark[cur] == 1 :
-                        cur = (cur + 1) % sz
+                idx, current = 0, 0
+                size = len(neighbours)
+                l = []
+                mark = [0]*size
+                count = 0
+                while count < len(neighbours):
+                    e = self.energy_difference(state, neighbours[current])
+                    if mark[current] == 1 :
+                        current = (current + 1) % size
                         continue
-                    if neighbours[cur].puzzleState in visited_state :
-                        mark[cur] = 1
-                        cnt += 1
+                    if neighbours[current].puzzleState in state_visited :
+                        mark[current] = 1
+                        count += 1
                     elif e <= 0:
-                        mark[cur] = 1
-                        cnt += 1
-                        state_parent[neighbours[cur].puzzleState] = current_state
-                        li.append(neighbours[cur].puzzleState)
+                        mark[current] = 1
+                        count += 1
+                        parent_state[neighbours[current].puzzleState] = current_state
+                        l.append(neighbours[current].puzzleState)
                     elif exp(-e / self.temperature) < random.uniform(0, 1):
-                        mark[cur] = 1
-                        cnt += 1
-                        state_parent[neighbours[cur].puzzleState] = current_state
-                        li.append(neighbours[cur].puzzleState)
-                    cur = (cur+1)%sz
-                li.reverse()
-                stack.extend(li)
+                        mark[current] = 1
+                        count += 1
+                        parent_state[neighbours[current].puzzleState] = current_state
+                        l.append(neighbours[current].puzzleState)
+                    current = (current+1)%size
+                l.reverse()
+                stack.extend(l)
 
         return 1
 
     def linear_strategy(self):
-        self.temperature = abs(self.temperature_max + self.x)
+        self.temperature = abs(self.max_temperature + self.x)
         self.x += self.step
 
     def random_strategy(self):
-        self.temperature = random.uniform(0, 1) * abs(self.temperature_max + self.x)
+        self.temperature = random.uniform(0, 1) * abs(self.max_temperature + self.x)
         self.x += self.step
 
-    def neg_exponential(self):
-        self.temperature = exp(-1 * self.x) * self.temperature_max
+    def negative_exponential(self):
+        self.temperature = exp(-1 * self.x) * self.max_temperature
         self.x += self.step
 
-    def pos_exponential(self):
-        self.temperature = exp(self.x) * self.temperature_max
+    def positive_exponential(self):
+        self.temperature = exp(self.x) * self.max_temperature
         self.x += self.step
--- Lab-4/SimulatedAnnealing/main_simulated_annealing.py
+++ Lab-4/SimulatedAnnealing/main_simulated_annealing.py
@@ -3,14 +3,15 @@ import Heuristic
 from datetime import datetime
 from Utils import *
 
+# Writes output to the file
 def write_to_file(file, heuristic_choice, start_state, goal_state, cooling_function, temp):
-    file.write("Heuristic Chosen : ")
+    file.write("Chosen Heuristic: ")
     if heuristic_choice == 1:
-        file.write("number of tiles displaced from their destined position \n")
+        file.write("Number of tiles displaced from their final position: \n")
     elif heuristic_choice == 2:
-        file.write("Total Manhatton distance \n")
+        file.write("Total Manhattan distance: \n")
     else:
-        file.write("Total Manhatton distance * number of tiles displaced from their destined position\n")
+        file.write("Total Manhattan distance * Number of tiles displaced from their final position:\n")
     file.write("Start State : \n")
     file.write(start_state[:3] + "\n" + start_state[3:6] + "\n" + start_state[6:] + "\n")
     file.write("Goal State : \n")
@@ -20,55 +21,59 @@ def write_to_file(file, heuristic_choice, start_state, goal_state, cooling_funct
     elif cooling_function == 2:
         file.write("Cooling Function : Random Strategy \n")
     elif cooling_function == 3:
-        file.write("Cooling Function : Negative exponential Function \n")
+        file.write("Cooling Function : Negative Exponential Function \n")
     else:
-        file.write("Cooling Function : Positive exponential function \n")
+        file.write("Cooling Function : Positive Exponential function \n")
     file.write("TMAX : {}\n".format(str(temp)))
     file.close()
 
 
+# Prints the menu for the user
 def start(startState, goalState, file):
-    print("Enter Choice depending on Heuristic :")
-    print("1.h1(n) = number of tiles displaced from their destined position")
-    print("2.h​2 ​(n)= Total Manhatton distance")
-    print("3.h ​3 ​(n)= h ​1 ​(n) * h ​2 ​(n)")
+    print("Enter your choice depending on the heuristic :")
+    print("1. h1(n) = Number of tiles displaced from their final position")
+    print("2. h2(n) = Total Manhattan distance")
+    print("3. h3​(n) = h1​(n) * h2(n)")
 
-    HeuristicChoice = int(input("Waiting for Choice: "))
-    if HeuristicChoice == 4:
-    	HeuristicChoice = 3
+    heuristic_choice = int(input("Enter your choice... "))
+    if heuristic_choice == 4:
+    	heuristic_choice = 3
 
-    is_tile_included = int(input("Enter 1 if consider tile as another tile else 0: "))
-    if is_tile_included:
+    is_blank_tile_included = int(input("Enter 1 so as to consider blank tile as another tile else enter 0... "))
+    if is_blank_tile_included:
         Heuristic.isTileInclude = True
-    print("Enter choice depending on cooling function:\n 1.Linear Function \n 2.Random Strategy \n"
-          " 3.Negative exponential Function \n 4.Positive exponential function\n")
-    cooling_function_choice = int(input("Waiting for Choice: "))
-    initial_temperature = int(input("Enter TMAX: "))
+    print("Enter choice depending on the cooling function:\n 1.Linear Function \n 2.Random Strategy \n"
+          " 3.Negative Exponential Function \n 4.Positive Exponential function\n")
+    cooling_function_choice = int(input("Enter your choice... "))
+    initial_temperature = int(input("Enter the value of TMAX... "))
 
-    write_to_file(file, HeuristicChoice, startState, goalState, cooling_function_choice, initial_temperature)
+    write_to_file(file, heuristic_choice, startState, goalState, cooling_function_choice, initial_temperature)
 
     start_time = datetime.now()
     puzzle_solver = simulated_annealing(startState, goalState, initial_temperature)
-    status = puzzle_solver.solve_eight_puzzle(startState, HeuristicChoice, cooling_function_choice)
+    search_status = puzzle_solver.solve_eight_puzzle(startState, heuristic_choice, cooling_function_choice)
     file = open("simulated_annealing_result.txt", "a")
-    if status == 1:
+    if search_status == 1:
         file.write("Search Status : Failed\n")
     file.write("Time Taken : {} ".format(str(datetime.now() - start_time)))
     file.close()
 
-# Starting point of the program.
+# Driver function to run the main program
 if __name__ == '__main__':
     startState = ""
     goalState = ""
 
+    # Opens the output file in write mode
     file = open("simulated_annealing_result.txt", "w+")
 
+    # Convert the start state to string form for easy manipulation
     with open("A*StartState") as f:
         for line in f:
             line = line.strip()
             line = line.replace(" ", "")
             startState += line
 
+    # Convert the goal state to string form for easy manipulation
     with open("A*GoalState") as f:
         for line in f:
             line = line.strip()
@@ -78,5 +83,5 @@ if __name__ == '__main__':
     startState = replaceTB(startState)
     goalState = replaceTB(goalState)
 
-    # start the program.
+    # Execute the program
     start(startState, goalState, file)
